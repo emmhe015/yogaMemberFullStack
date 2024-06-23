b9484a5b0f4dfd782d89d4127d0d861abe7d0b26
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-23 12:13:50 +0000

    Update admin.py with classes

--- yogaapp/admin.py
+++ yogaapp/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Profile, YogaClass, Booking, Comment
 
-admin.site.register(Profile)
-admin.site.register(YogaClass)
-admin.site.register(Booking)
-admin.site.register(Comment)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'profile_picture')
+
+@admin.register(YogaClass)
+class YogaClassAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_time', 'type', 'max_members')
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'yoga_class', 'booking_date')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'yoga_class', 'text', 'created_at')
