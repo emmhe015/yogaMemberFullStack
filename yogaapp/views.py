dd615d6386b74e043582123ae83bbca46b3cf5a9
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-23 08:41:06 +0000

    Add login to views.py

--- yogaapp/views.py
+++ yogaapp/views.py
@@ -11,4 +11,22 @@ from django.contrib import messages
 def home(request):
     classes = YogaClass.objects.all()
     return render(request, 'yogaapp/index.html', {'classes': classes})
-    
+
+# login
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid username or password')
+        else:
+            messages.error(request, 'Invalid username or password')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'yogaapp/login.html', {'form': form})
