bc316dd706491569649d7a94d5e63bd7adc5808d
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-23 08:38:58 +0000

    Add home section in viewa.py

--- yogaapp/views.py
+++ yogaapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .models import YogaClass, Booking, Comment
+from django.contrib import messages
+
+# homepage
+def home(request):
+    classes = YogaClass.objects.all()
+    return render(request, 'yogaapp/index.html', {'classes': classes})
+    
