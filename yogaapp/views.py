fe288fefe3885235bc91a78430e2989b21918d05
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-23 08:43:42 +0000

    Add registration to views.py

--- yogaapp/views.py
+++ yogaapp/views.py
@@ -30,3 +30,18 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, 'yogaapp/login.html', {'form': form})
+
+# registration
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'yogaapp/register.html', {'form': form})
