7f98578da2c2368f215921a768670c8a8ba2f362
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-25 12:17:56 +0000

    Add accountsconfig class to apps.py

--- yogaapp/apps.py
+++ yogaapp/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class StudioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'yogaapp'
+
+class AccountsConfig(AppConfig):
+    name = 'accounts'
+
+    def ready(self):
+        import accounts.signals
