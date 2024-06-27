656c22e4b6e3126f3573988ac2c885d688c6df06
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-27 09:37:22 +0000

    Add herokuapp to allowedhosts

--- yogastudio/settings.py
+++ yogastudio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hug-d()2t%zu3nf1q9dq_7f^_c3!-56ikuf4x&t=h73#^#j+)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '8000-emmhe015-yogamemberfull-r38pj0e6pmx.ws-eu114.gitpod.io', '127.0.0.1:8000/admin/'
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '8000-emmhe015-yogamemberfull-r38pj0e6pmx.ws-eu114.gitpod.io', '127.0.0.1:8000/admin/', '.herokuapp.com'
 ]
 
 
