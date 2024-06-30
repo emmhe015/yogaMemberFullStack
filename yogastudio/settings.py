9a5b096cda8b37e7354034dd73d513b0e1fb0356
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-30 09:27:08 +0000

    Update secretkey

--- yogastudio/settings.py
+++ yogastudio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hug-d()2t%zu3nf1q9dq_7f^_c3!-56ikuf4x&t=h73#^#j+)2'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
