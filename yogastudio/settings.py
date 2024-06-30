adaae134a3597f2e61ddfedd0c123c40c31531e7
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-30 10:49:27 +0000

    Create staticfiles

--- yogastudio/settings.py
+++ yogastudio/settings.py
@@ -155,6 +155,7 @@ MEDIA_ROOT = [BASE_DIR / 'media',]
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "yogaapp/static",]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
