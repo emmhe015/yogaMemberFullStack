bf3c6fb5c3cef0f48494e8ac1856df10dbc5d59f
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-30 09:46:45 +0000

    Add gitpod to trusted in settings.py

--- yogastudio/settings.py
+++ yogastudio/settings.py
@@ -112,7 +112,8 @@ DATABASES = {
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
-    "https://*.herokuapp.com"
+    "https://*.herokuapp.com",
+    'https://*.gitpod.io'
 ]
 
 # Password validation
