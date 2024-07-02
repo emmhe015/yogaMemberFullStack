fd6e4a404592383f4a9928d3af12158732934e5d
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-02 09:56:19 +0000

    Create accounts app

--- yogastudio/settings.py
+++ yogastudio/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    "accounts",
     'django_cleanup.apps.CleanupConfig',  # Ensure to clean up image files
     'django.contrib.sites',  # Required for authentication
     'allauth',
