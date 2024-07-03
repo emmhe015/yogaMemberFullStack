059070fefc68af7e43bbb1053fe995847a51f840
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-03 08:33:39 +0000

    Add updateprofile in forms.py

--- yogaapp/forms.py
+++ yogaapp/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -13,6 +15,28 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
 class ProfileUpdateForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(), required=False)
+    password_confirmation = forms.CharField(widget=forms.PasswordInput(), required=False)
+
     class Meta:
-        model = Profile
-        fields = ['profile_picture']
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email is already in use.")
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirmation = cleaned_data.get("password_confirmation")
+
+        if password and password != password_confirmation:
+            self.add_error('password_confirmation', "Passwords do not match.")
+        
+        if password:
+            validate_password(password)
+        
+        return cleaned_data
