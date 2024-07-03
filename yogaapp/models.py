fdef5eb124193b67b865fba7215026eedef19361
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-03 11:04:16 +0000

    Add liveclass to models.py

--- yogaapp/models.py
+++ yogaapp/models.py
@@ -31,6 +31,15 @@ class YogaClass(models.Model):
     def __str__(self):
         return f'{self.title} - {self.type}'
 
+class LiveClass(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     live_class = models.ForeignKey(LiveClass, on_delete=models.CASCADE)
