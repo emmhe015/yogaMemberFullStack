2b7e7e627b5ac25de2437abab7266143e7b2d632
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-10 11:01:54 +0000

    Fix url path to delete comment

--- yogaapp/urls.py
+++ yogaapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('booking/<int:class_id>/', views.booking_view, name='booking'),
     path('add_comment/<int:class_id>/', views.add_comment, name='add_comment'),
-    path('delete_comment/<int:comment_id>/', delete_comment, name='delete_comment'),
+    path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
     path('summernote/', include('django_summernote.urls')),
     path('home_logged_in/', views.home_logged_in, name='home_logged_in'),
     path('profile/update/', views.update_profile, name='update_profile'),
