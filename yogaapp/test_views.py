e66395ec3138a3506fc20a26a052f65646983d11
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-13 08:16:39 +0000

    Test homeloggedin view ok

--- yogaapp/test_views.py
+++ yogaapp/test_views.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from yogaapp.models import LiveClass, Booking
 
 class HomeViewTest(TestCase):
     def test_home_view(self):
@@ -9,3 +12,31 @@ class HomeViewTest(TestCase):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+class HomeLoggedInViewTest(TestCase):
+    def setUp(self):
+        """
+        Set up test data.
+        """
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.live_class1 = LiveClass.objects.create(title='Yoga Class 1', date='2024-07-10', time='10:00:00', description='Description 1')
+        self.live_class2 = LiveClass.objects.create(title='Yoga Class 2', date='2024-07-11', time='11:00:00', description='Description 2')
+        Booking.objects.create(user=self.user, live_class=self.live_class1)
+
+    def test_home_logged_in_view(self):
+        """
+        Test that the home_logged_in view renders correctly for a logged-in user.
+        """
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('home_logged_in'))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertIn('live_classes', response.context)
+        self.assertIn('booked_classes', response.context)
+        
+        live_classes = response.context['live_classes']
+        booked_classes = response.context['booked_classes']
+
+        self.assertEqual(list(live_classes), [self.live_class1, self.live_class2])
+        self.assertEqual(list(booked_classes), [self.live_class1])
