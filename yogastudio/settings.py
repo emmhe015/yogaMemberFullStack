a54dd89b57d46b4903770984dd0fb270fc56070d
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-13 13:11:41 +0000

    Cange DEBUG to False

--- yogastudio/settings.py
+++ yogastudio/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-emmhe015-yogamemberfull-r38pj0e6pmx.ws-eu115.gitpod.io', '127.0.0.1', 'localhost', '127.0.0.1:8000/admin/', '.herokuapp.com',
 ]
