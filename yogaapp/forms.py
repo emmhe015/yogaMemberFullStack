b80e753f125e5f6bc06578f754a7b6eee96068d4
Emma Henriksson <emmaha.eh@gmail.com>
2024-07-13 06:43:17 +0000

    Update import order in forms.py

--- yogaapp/forms.py
+++ yogaapp/forms.py
@@ -1,10 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
-from .models import LiveClass
+from .models import LiveClass, Profile
 
 
 class UserRegisterForm(UserCreationForm):
